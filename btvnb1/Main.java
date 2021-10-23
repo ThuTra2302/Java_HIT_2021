7f4c0a504948a575302504ef28066cc5239e7705
ThuTra2302 <Thutrale156@gmail.com>
2021-10-23 21:27:31 +0700

    btvn 1

---
+++ btvnb1/Main.java
@@ -0,0 +1,21 @@
+package btvnb1;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n, m;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ btvnb1/bai2.java
@@ -0,0 +1,24 @@
+package btvnb1;
+public class bai2 {
+    public static void main(String[] args) {
+        System.out.println("*       *     *      * * * *    * * * *    *     *      ");
+        System.out.println("|       |    / \\     |       *  |       *   \\   /     ");
+        System.out.println("* ----- *   *---*    * * * *    * * * *       *      ");
+        System.out.println("|       |  /     \\   |          |             |   ");
+        System.out.println("*       * *       *  *          *             *    ");
+        System.out.println();
+        System.out.println();
+        System.out.println("*     *  ******  *          *         * ");
+        System.out.println("| \\   |  |        \\        / \\       / ");
+        System.out.println("*   * *  ******     *    *    *     * ");
+        System.out.println("|    \\|  |           \\  /       \\  / ");
+        System.out.println("*     *  ******        *          *  ");
+        System.out.println();
+        System.out.println();
+        System.out.println(" *     * ******      *      * * * * ");
+        System.out.println("  \\   /  |          / \\     |       * ");
+        System.out.println("    *    ******    *---*    * * * * ");
+        System.out.println("    |    |        /     \\   |   \\  ");
+        System.out.println("    *    ******  *       *  *     * ");
+    }
+}
